0030cb3d0547a581d5f8cfffb3b9b609ecb9f765
B. Petersen <r10s@b44t.com>
2023-05-24 14:19:33 +0200

    work

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 
-
-
-var map = L.map('map', {doubleClickZoom: true, zoomControl: false, /* added manually below */ tapHold: true, })
-    .setView([53.785054, 9.408707], 15);
+var map = L.map('map', {
+        doubleClickZoom: true,
+        zoomControl: false, /* added manually below */
+        tapHold: true
+    }).setView([53.785054, 9.408707], 15);
 map.attributionControl.setPrefix('');
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19, attribution: "&copy; OpenStreetMap" }).addTo(map);
 L.control.scale({position: 'bottomleft'}).addTo(map);
 L.control.zoom({position: 'topright'}).addTo(map);
-var tracks = {};
-var contacts = {};
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: "&copy; OpenStreetMap"
+    }).addTo(map);
+
+var tracks = {};   // hash contactId to positions, last position == newest position
+var contacts = {}; // hash contactId to info
+
 
 
 window.webxdc.setUpdateListener((update) => {
@@ -19,11 +26,15 @@ window.webxdc.setUpdateListener((update) => {
             if (label.length > 10) {
                 label = label.substring(0, 9).trim() + ".."
             }
-            labelHtml = htmlentities(label);
 
             var marker = L.marker(payload).addTo(map);
-            marker.bindTooltip(labelHtml, {permanent: true, direction: 'bottom', offset: [-15, 15], className: 'transparent-tooltip'}).openTooltip();
-            marker.bindPopup(popupHtml(payload), {closeButton: false});
+            marker.bindTooltip(htmlentities(label), {
+                    permanent: true,
+                    direction: 'bottom',
+                    offset: [-15, 15],
+                    className: 'transparent-tooltip'
+                }).openTooltip();
+            marker.bindPopup(popupHtml(payload), { closeButton: false });
         } else {
             if (!Array.isArray(tracks[payload.contactId])) {
                 tracks[payload.contactId] = [];
@@ -36,25 +47,29 @@ window.webxdc.setUpdateListener((update) => {
     updateTracks();
 });
 
-function popupHtml(payload) {
-    return '<div>' + htmlentities(payload.text) + '</div>'
-        + '<div><small>'
-        + payload.lat.toFixed(4) + '°/' + payload.lng.toFixed(4) + '°<br>'
-        + htmlentities(new Date(payload.timestamp*1000).toLocaleString())
-        + '</small></div>';
-}
+
 
 // contact's tracks
 
 function updateTracks() {
     for (contactId in tracks) {
         L.polyline(tracks[contactId], {color: contacts[contactId].color, weight: 4}).addTo(map);
-        var marker = L.marker({lat: tracks[contactId][0][0], lng: tracks[contactId][0][1]}).addTo(map);
-        marker.bindTooltip('<span style="color:'+contacts[contactId].color+'">' + htmlentities(contacts[contactId].text) + '</span>',
-            {permanent: true, direction: 'bottom', offset: [-15, 15], className: 'transparent-tooltip'}).openTooltip();
-        marker.bindPopup(popupHtml(contacts[contactId]), {closeButton: false});
+
+        var lastMarker = tracks[contactId].length - 1;
+        var marker = L.marker({
+                lat: tracks[contactId][lastMarker][0],
+                lng: tracks[contactId][lastMarker][1]
+            }).addTo(map);
+        marker.bindTooltip('<span style="color:'+contacts[contactId].color+'">' + htmlentities(contacts[contactId].text) + '</span>', {
+                permanent: true,
+                direction: 'bottom',
+                offset: [-15, 15],
+                className: 'transparent-tooltip'
+            }).openTooltip();
+        marker.bindPopup(popupHtml(contacts[contactId]), { closeButton: false });
     }
 }
+
 
 
 // share a dedicated location
@@ -74,9 +89,7 @@ function onSend() {
                     lng: popupLatlng.lng,
                     text: elem.value,
                 },
-            },
-            'POI added to map at ' + popupLatlng.lat.toFixed(4) + '/' + popupLatlng.lng.toFixed(4) + ' with text: ' + value
-        );
+            }, 'POI added to map at ' + popupLatlng.lat.toFixed(4) + '/' + popupLatlng.lng.toFixed(4) + ' with text: ' + value);
     }
 }
 
@@ -92,8 +105,17 @@ function onMapLongClick(e) {
 map.on('contextmenu', onMapLongClick);
 
 
+
 // tools
 
 function htmlentities(rawStr) {
     return rawStr.replace(/[\u00A0-\u9999<>\&]/g, ((i) => `&#${i.charCodeAt(0)};`));
+}
+
+function popupHtml(payload) {
+    return '<div>' + htmlentities(payload.text) + '</div>'
+        + '<div><small>'
+        + payload.lat.toFixed(4) + '°/' + payload.lng.toFixed(4) + '°<br>'
+        + htmlentities(new Date(payload.timestamp*1000).toLocaleString())
+        + '</small></div>';
 }
